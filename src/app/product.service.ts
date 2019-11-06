d9b470f8a07ca27383f1ab78291624995eb3ed18
Ugwuanyi Ekene <love2ekene@gmail.com>
2019-11-06 03:56:19 -0800

    complete

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -9,7 +9,9 @@ import { Product } from './product';
 export class ProductService {
 
   private _albumUrl = '../assets/album.json';
+
   private _productsUrl = '../assests/products.json';
+
   constructor(private _http: Http) { }
 
   getAlbum(id: number): Observable<Album> {
@@ -17,6 +19,7 @@ export class ProductService {
 
   }
   getProducts(): Observable<Product[]> {
-    return this._http.get(this._productsUrl).map(response => <Product[]>response.json());
+    return this._http.get(this._productsUrl).map(response => 
+      <Product[]>response.json());
   }
 }
